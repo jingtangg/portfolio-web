d82525495a1aae43bda6b7f553ca139e987566d4
jingtangg <jingtang1927@gmail.com>
2025-10-07 09:50:38 +0800

    feat:首頁定版

--- app/app.config.ts
+++ app/app.config.ts
@@ -16,14 +16,14 @@ export default defineAppConfig({
     },
     pageHero: {
       slots: {
-        container: 'py-18 sm:py-24 lg:py-32',
+        container: 'py-8 sm:py-12 lg:py-16',
         title: 'mx-auto max-w-xl text-pretty text-3xl sm:text-4xl lg:text-5xl',
         description: 'mt-2 text-md mx-auto max-w-2xl text-pretty sm:text-md text-muted'
       }
     }
   },
   footer: {
-    credits: `Built with Nuxt UI • © ${new Date().getFullYear()}`,
+    credits: `All rights reserved © ${new Date().getFullYear()} Jing`,
     colorMode: false,
     links: [{
       'icon': 'i-heroicons-solid-camera',
